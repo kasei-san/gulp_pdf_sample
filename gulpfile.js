10d0a2045f0cec7492450331527c52c06a493b54
kasei_san <kasei.san@gmail.com>
2014-04-28 11:36:38 +0900

    Add comment

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,24 @@ var markdown    = require('gulp-markdown');
 var markdownpdf = require('gulp-markdown-pdf');
 var clean       = require('gulp-clean');
 
+// default task do clean, html and pdf task
 gulp.task('default', ['clean', 'html', 'pdf']);
 
+// markdown to html
 gulp.task('html', function () {
   return gulp.src('intro.md')
     .pipe(markdown())
     .pipe(gulp.dest('dist/html'));
 });
 
+// markdown to pdf
 gulp.task('pdf', function () {
   return gulp.src('intro.md')
     .pipe(markdownpdf())
     .pipe(gulp.dest('dist/pdf'));
 });
 
+// cleanup dist dir
 gulp.task('clean', function () {
   return gulp.src('dist')
     .pipe(clean());
